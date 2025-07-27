aa66c231399fb0f5c2fa4608f970b26e35db1c58
Andrey Kotsenko <kocenkoandrej14@gmail.com>
2025-07-27 15:23:41 +0300

    Basic app

---
+++ test_app.py
@@ -0,0 +1,13 @@
+from app import app
+
+def test_home():
+    with app.test_client() as client:
+        res = client.get("/")
+        assert res.status_code == 200
+        assert res.get_json()["message"] == "Hello, DevOps World!"
+
+def test_health():
+    with app.test_client() as client:
+        res = client.get("/health")
+        assert res.status_code == 200
+        assert res.get_json()["status"] == "ok"
